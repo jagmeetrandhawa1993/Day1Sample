9e8fd2cb8f3ffd45cd2f1dfccb21835eebc06ef0
Jagmeet Kaur <jagmeetkaur@jagmeets-macbook-air.local>
2019-03-04 18:47:10 -0500

    Initial Commit

---
+++ Day1Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day1Sample
+//
+//  Created by Jagmeet Kaur on 2019-03-04.
+//  Copyright © 2019 Jagmeet Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
