3a395d8702020597712f937920306d3f77cda775
Jagmeet Kaur <jagmeetkaur@jagmeets-macbook-air.local>
2019-03-04 21:17:08 -0500

    Calculations

--- Day1Sample/ViewController.swift
+++ Day1Sample/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var lblMessage1: UILabel!
     @IBOutlet weak var lblMessage: UILabel!
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var txtNumber1: UITextField!
+    
+    @IBOutlet weak var txtNumber2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +24,19 @@ class ViewController: UIViewController {
         lblMessage.text = "Hello World"
     }
 
-
+    @IBAction func btnSubmitClick(_ sender: UIButton) {
+        lblMessage.text = "I am from button text"
+        
+    }
+    
+    @IBAction func btnAddClick(_ sender: UIButton) {
+        var a = Int(txtNumber1.text!)
+        var b = Int(txtNumber2.text!)
+        
+      let c = a! + b!
+        
+       // lblResult.text = String( c)
+        lblResult.text = "Add : \(c)"
+    }
 }
 
